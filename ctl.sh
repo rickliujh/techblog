201a432aa95d6fa412dc096ad60c5c5fcfaf5f2c
Rick <rick.ryan2@outlook.com>
2024-05-12 20:42:24 +0000

    Quartz sync: May 12, 2024, 8:42 PM

--- ctl.sh
+++ ctl.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # -e: exit on error
 # -u: exit on unset variables
 set -eu
 
+NODE_VERSION=lts-bookworm
+
 preview() {
-    docker run --rm -v ${pwd}:\workspace -w \workspace -itp 8080:8080 yarn node:lts-slim quartz build --serve
+    docker run --rm -v $(pwd):/workspace -w /workspace -itp 8080:8080 node:$NODE_VERSION yarn quartz build --serve
 }
 
 # For full help options, you can run npx quartz sync --help.
@@ -27,26 +29,24 @@ quartz() {
         -u 1000:1000 \
         -v $(pwd):/home/node/quartz -v ~/.ssh:/home/node/.ssh \
         -w /home/node/quartz \
-        node:lts-bookworm \
-        /bin/bash -c "su node; yarn quartz $@"
+        node:$NODE_VERSION \
+        /bin/bash -c "yarn quartz $@"
 }
 
 show_menu() {
-    echo -e "\u001b[32;1m Setting up your env with dots2k...\u001b[0m"
-    echo -e " \u001b[37;1m\u001b[4mSelect an option:\u001b[0m"
-    echo -e "  \u001b[34;1m (0) Setup Everything \u001b[0m"
-    echo -e "  \u001b[34;1m (1) Install Packages \u001b[0m"
-    echo -e "  \u001b[34;1m (2) Install Languages \u001b[0m"
-    echo -e "  \u001b[34;1m (3) Install Extras \u001b[0m"
-    echo -e "  \u001b[34;1m (4) Backup Configs \u001b[0m"
-    echo -e "  \u001b[34;1m (5) Setup Symlinks \u001b[0m"
-    echo -e "  \u001b[31;1m (*) Anything else to exit \u001b[0m"
-    echo -en "\u001b[32;1m ==> \u001b[0m"
+    echo -e "\u001b[32;1m QUARTZ CTL MENU \u001b[0m"
+    echo -e " \u001b[37;1m\u001b[4m Options:\u001b[0m"
+    echo -e "  \u001b[34;1m (0) perview | p => perview blog before publich \u001b[0m"
+    echo -e "  \u001b[34;1m (1) sync => sync blog to remote repository, or update quartz \u001b[0m"
+    echo -e "  \u001b[34;1m (2) sync --no-pull | sop => sync blog to remote repository but not update quartz \u001b[0m"
+    echo -e "  \u001b[34;1m (3) quartz | q => run any quartz support sub commands \u001b[0m"
 }
 
 main() {
     case "$1" in
     -m | --menu | m | menu | show_menu) show_menu ;;
+    p) preview;;
+    sop) sync --no-pull;;
     *) "$@";;
     esac
     exit 0
