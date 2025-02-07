600e16824b30a6bead3c56c4332fb5003a762615
Rick <rick.ryan2@outlook.com>
2025-02-07 22:18:13 +0000

    feat: enable link preview cards/picture

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,7 +19,7 @@ const config: QuartzConfig = {
     baseUrl: "rickliujh.github.io/techblog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: false,
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
